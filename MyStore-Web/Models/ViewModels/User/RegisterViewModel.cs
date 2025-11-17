8afe1d8f6a95df3bcebc283700fa8a49bb63a7b7
alipour <itsrahimalipour@gmail.com>
2025-11-17 14:09:31 +0330

    p

---
+++ MyStore-Web/Models/ViewModels/User/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStore_Web.Models.ViewModels.User
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = " نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string UserName { get; set; } = null!;
+
+        [Display(Name = " ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "لطفا یک {0} معتبر وارد کنید ")]
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = " رمزعبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "کلمات عبور باهم تطابق ندارند")]
+        [Display(Name = " تکرار رمزعبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string RePassword { get; set; } = null!;
+    }
+}
